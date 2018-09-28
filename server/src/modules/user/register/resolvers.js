8c9fb649a53c7b646d701590f5e0509b5e5f7353
ahrbil <ahrbil.me@gmail.com>
2018-09-28 12:08:11 +0100

    updated dependencies

--- server/src/modules/user/register/resolvers.js
+++ server/src/modules/user/register/resolvers.js
@@ -1,7 +1,13 @@
+
+
 const resolvers = {
-  Query: {
-    user: () => `registering a user`
+  Mutation: {
+    signup: async (_, {name,email,password}, ctx, info) => {
+      const isUserAlReadyExists = ctx.db.query.users(where:{})
+      return true;
+    }
   }
 };
 
 module.exports = resolvers;
+
